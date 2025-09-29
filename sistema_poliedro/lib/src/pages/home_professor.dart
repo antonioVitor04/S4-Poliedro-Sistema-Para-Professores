ada800c322183a30e0650575fb49fe9a3a05cec7
alvaro <alvarojunqueira881@gmail.com>
2025-09-29 12:58:22 -0300

    feat: login com backend funcionando

---
+++ sistema_poliedro/lib/src/pages/home_professor.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeProfessor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home - Professor")),
+      body: Center(child: Text("Bem-vindo, Professor!")),
+    );
+  }
+}
