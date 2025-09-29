1fb9be28b2d185873d204adc939c9ee909ba9885
Antônio Vítor Bozzo de Napoli <antonionapoli394@gmail.com>
2025-09-29 13:17:22 -0300

    arrumando env

--- sistema_poliedro/lib/src/pages/home_professor.dart
+++ sistema_poliedro/lib/src/pages/home_professor.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomeProfessor extends StatelessWidget {
+  const HomeProfessor({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
