3307e44e677432ea30da3048cbb5275598d0b3f5
alvaro <alvarojunqueira881@gmail.com>
2025-09-28 22:37:25 -0300

    feat: adicionando fontes, cores, e parte da tela de login

---
+++ sistema_poliedro/lib/src/styles/cores.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const azulClaro = Color(0xFF1EB4C3);
+  static const amarelo = Color(0xFFFBA41F);
+  static const vermelho = Color(0xFFED2152);
+  static const verdeConfirmacao = Color(0xFF27AE60);
+  static const vermelhoErro = Color(0xFFFE6658);
+}
